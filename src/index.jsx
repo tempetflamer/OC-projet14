c361663a34dc5bc6a9c4e58aeb560fd51ad13cae
tempet <tempetflamer@gmail.com>
2023-07-03 13:04:05 +0200

    create context folder Move EmployeeContext from utils to context folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import EmployeeContextProvider from './utils/EmployeeContext'
+import EmployeeContextProvider from './context/EmployeeContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
